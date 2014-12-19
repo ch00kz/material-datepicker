ca275bb00c6faacd826faf5a172ef34206ee608f
Andrew Thomas <andr3w.l.thomas@gmail.com>
2014-12-19 02:06:05 -0500

    removed print statement and changed offset a bit

--- material.datepicker.js
+++ material.datepicker.js
@@ -1,13 +1,12 @@
-function datepicker(field) {
+function DatePicker(field) {
 	var picker = $('<div class="material-datepicker hide"><section class="1></section></div>');
 	$(field).after(picker);
 	$(field).focus(function(){
-		console.log("I should be showing the datepicker now");
 		picker.removeClass('hide');
 	});
 
 	var fieldHeight = $(field).height();
-	var offsetTop = $(field).offset().top + fieldHeight + 15;
+	var offsetTop = $(field).offset().top + fieldHeight + 10;
 	var offsetLeft = $(field).offset().left;
 
 	picker.css('top', offsetTop);
