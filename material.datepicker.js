9f7589c17d2d91793d141c4a465cddea218464a2
Andrew Thomas <andr3w.l.thomas@gmail.com>
2014-12-20 16:50:18 -0500

    fixed bug preventing multiple pickers from being initialised

--- material.datepicker.js
+++ material.datepicker.js
@@ -1,6 +1,7 @@
 function DatePicker(field) {
 	var html =
-   ['<div class="material-datepicker hide">',
+   	[
+   	'<div class="material-datepicker hide">',
     	'<section class="top-day">',
     		'<span data-bind="text: day"></span>',
     	'</section>',
@@ -27,8 +28,8 @@ function DatePicker(field) {
     	'<section class="button-container">',
     		'<a class="close" data-bind="click: closePicker">OK</a>',
     	'</section>',
-    '</div>'
-   ].join('\n');
+	'</div>'
+   	].join('\n');
 
 	var picker = $(html);
 	$(field).after(picker);
@@ -115,9 +116,11 @@ function DatePicker(field) {
 	    self.day = ko.computed(function(){
 	    	return days[self.datePickerValue().getDay()];
 	    });
+
 	    self.date = ko.computed(function(){
 	    	return self.datePickerValue().getDate();
 	    });
+
 	    self.month = ko.computed(function(){
 	    	return months[self.datePickerValue().getMonth()];
 	    });
@@ -125,6 +128,7 @@ function DatePicker(field) {
 	    self.shortMonth = ko.computed(function(){
 	    	return monthShort[self.datePickerValue().getMonth()];
 	    });
+
 	    self.year = ko.computed(function(){
 	    	return self.datePickerValue().getFullYear();
 	    });
@@ -162,18 +166,8 @@ function DatePicker(field) {
 
 	    self.buildMonthStruct();
 	}
-
-	var app = new AppViewModel(field, picker);
-	window.app = app;
-	// Activates knockout.js
-	ko.applyBindings(app);
-}
-
-function Calendar(year){
-	this.months = []
-	for(var i = 1; i < 13; i++){
-		this.months.push(new Month(i, year));
-	}
+	var viewModel = new AppViewModel(field, picker);
+	ko.applyBindings(viewModel, picker[0]);
 }
 
 function Month(number, year) {
@@ -198,8 +192,6 @@ var days = [
 			'Thursday', 'Friday', 'Saturday'
 		];
 
-
-
 var months = [
 	'January', 'February', 'March', 'April', 'May',
 	'June', 'July', 'August', 'September', 'October',
@@ -207,9 +199,7 @@ var months = [
 ];
 
 var monthShort = [
-	'Jan', 'Feb', 'Ma', 'Apr', 'May',
+	'Jan', 'Feb', 'Mar', 'Apr', 'May',
 	'June', 'July', 'Aug', 'Sept', 'Oct',
 	'Nov', 'Dec'
 ];
-
-
